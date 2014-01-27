703ae76b4c3943c46237570447c2f913b7f89765
Alex Shvid <a@shvid.com>
2014-01-27 15:49:49 -0800

    javadocs

---
+++ copy.sh
@@ -0,0 +1,5 @@
+rm -rf cassandra-cql/javadoc
+cp -rf ../spring-data-cassandra/cassandra-cql/target/javadoc cassandra-cql/
+
+rm -rf cassandra/javadoc
+cp -rf ../spring-data-cassandra/cassandra/target/javadoc cassandra/
