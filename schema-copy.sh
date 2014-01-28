f2396d0b5969d3769c204191ccc0c4b3462d8ada
Alex Shvid <a@shvid.com>
2014-01-27 16:37:54 -0800

    schema cassandra

---
+++ schema-copy.sh
@@ -0,0 +1,3 @@
+rm -rf schema/cassandra/*
+cp ../spring-data-cassandra/cassandra-cql/src/main/resources/org/springdata/cassandra/config/cassandra-cql-1.0.xsd schema/cassandra/
+cp ../spring-data-cassandra/cassandra/src/main/resources/org/springdata/cassandra/config/cassandra-1.0.xsd schema/cassandra/
