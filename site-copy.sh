fc261084f144e6be380d24b8670b2af875fbe1e7
Alex Shvid <a@shvid.com>
2014-02-03 22:35:50 -0800

    update javadocs

--- site-copy.sh
+++ site-copy.sh
@@ -5,6 +5,6 @@ cp -rf ../spring-data-cassandra/target/site sdc
 cp -rf ../spring-data-cassandra/cassandra-cql/target/site sdc/spring-cassandra-cql
 cp -rf ../spring-data-cassandra/cassandra/target/site sdc/spring-data-cassandra
 
-cp -rf ../spring-data-cassandra/cassandra-cql/target/javadoc sdc/cassandra-cql/javadoc
+cp -rf ../spring-data-cassandra/cassandra-cql/target/javadoc sdc/spring-cassandra-cql/javadoc
 cp -rf ../spring-data-cassandra/cassandra/target/javadoc sdc/spring-data-cassandra/javadoc
 
