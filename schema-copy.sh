fc323480e1072e4031387919475f9f3f9d50b065
Alex Shvid <a@shvid.com>
2014-04-15 13:29:33 -0700

    update schema

--- schema-copy.sh
+++ schema-copy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 rm -rf schema/cassandra/*
 cp ../spring-data-cassandra/cassandra-cql/src/main/resources/org/springdata/cassandra/config/cassandra-cql-1.0.xsd schema/cassandra/
 cp ../spring-data-cassandra/cassandra/src/main/resources/org/springdata/cassandra/config/cassandra-1.0.xsd schema/cassandra/
+
+rm -rf schema/ehcache/*
+cp ../spring-data-ehcache/src/main/resources/org/springdata/ehcache/config/spring-ehcache-1.0.xsd schema/ehcache/
+
