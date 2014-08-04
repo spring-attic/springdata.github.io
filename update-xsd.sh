f58a03c6cd2e43aa2801a2c59c86395e76aa6432
Alex Shvid <a@shvid.com>
2014-08-04 16:59:27 -0700

    update cassandra schemas

--- update-xsd.sh
+++ update-xsd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rf schema/cassandra/*
-cp ../spring-data-cassandra/cassandra-cql/src/main/resources/org/springdata/cassandra/config/cassandra-cql-1.0.xsd schema/cassandra/
-cp ../spring-data-cassandra/cassandra/src/main/resources/org/springdata/cassandra/config/cassandra-1.0.xsd schema/cassandra/
+cp ../spring-data-cassandra/cql/src/main/resources/org/springdata/cql/config/cql-1.0.xsd schema/cql-1.0.xsd 
+cp ../spring-data-cassandra/cql/src/main/resources/org/springdata/cql/config/cql-1.0.xsd schema/cql.xsd
+
+cp ../spring-data-cassandra/cassandra/src/main/resources/org/springdata/cassandra/config/cassandra-1.0.xsd schema/cassandra-1.0.xsd
+cp ../spring-data-cassandra/cassandra/src/main/resources/org/springdata/cassandra/config/cassandra-1.0.xsd schema/cassandra.xsd
 
 rm -rf schema/ehcache/*
 cp ../spring-data-ehcache/src/main/resources/org/springdata/ehcache/config/spring-ehcache-1.0.xsd schema/ehcache/
