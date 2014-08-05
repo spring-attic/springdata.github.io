f34f097b083cdeba3b23086a0cf9c3a6402788ec
Alex Shvid <a@shvid.com>
2014-08-04 20:50:48 -0700

    update site

--- site-copy.sh
+++ site-copy.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 
 rm -rf sdc
 
 cp -rf ../spring-data-cassandra/target/site sdc
-cp -rf ../spring-data-cassandra/cassandra-cql/target/site sdc/spring-cassandra-cql
+cp -rf ../spring-data-cassandra/cql/target/site sdc/spring-data-cql
 cp -rf ../spring-data-cassandra/cassandra/target/site sdc/spring-data-cassandra
 
-cp -rf ../spring-data-cassandra/cassandra-cql/target/javadoc sdc/spring-cassandra-cql/javadoc
+cp -rf ../spring-data-cassandra/cql/target/javadoc sdc/spring-data-cql/javadoc
 cp -rf ../spring-data-cassandra/cassandra/target/javadoc sdc/spring-data-cassandra/javadoc
 
